e20ea203441c079ee7fc87c1b33077ce301d5368
ommmishra <ommmishra830gmail.com>
2019-05-14 17:02:55 +0530

    added Rnadom Initialization

--- machine-learning-ex4/ex4/randInitializeWeights.m
+++ machine-learning-ex4/ex4/randInitializeWeights.m
@@ -11,7 +11,7 @@ function W = randInitializeWeights(L_in, L_out)
 
 % You need to return the following variables correctly 
 W = zeros(L_out, 1 + L_in);
-
+epsilon_init = 0.12;
 % ====================== YOUR CODE HERE ======================
 % Instructions: Initialize W randomly so that we break the symmetry while
 %               training the neural network.
@@ -20,7 +20,7 @@ W = zeros(L_out, 1 + L_in);
 %
 
 
-
+w = rand(L_out, 1 + L_in) * (2 * epsilon_init) - epsilon_init;
 
 
 
